cfc0d303f7c0e7776fb7286bf68b8e5250b05c55
seongyojeong <seongyojeong1229@gmail.com>
2025-04-24 22:44:39 +0900

    Create or_gate_d_tb.v

---
+++ Basic/Gates/or_gate/Data Flow Modeling/or_gate_d_tb.v
@@ -0,0 +1,27 @@
+module or_gate_d_tb;
+    reg a;
+    reg b;
+    wire y;
+
+    or_gate_d DUT(
+        .a(a),
+        .b(b),
+        .y(y)
+    );
+
+    initial begin
+        a = 0; b = 0;
+        #50;
+        
+        a = 0; b = 1;
+        #50;
+        
+        a = 1; b = 0;
+        #50;
+        
+        a = 1; b = 1;
+        #50;
+        
+        $finish;
+    end
+endmodule
