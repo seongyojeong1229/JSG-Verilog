c3d27e8907e2f899c5240cfdda9a66aad90fe701
seongyojeong <seongyojeong1229@gmail.com>
2025-04-24 22:17:18 +0900

    Create and_gate_b_tb.v

---
+++ Basic/Gates/and_gate/Behavior Modeling/and_gate_b_tb.v
@@ -0,0 +1,32 @@
+module and_gate_b_tb;
+    reg a;
+    reg b;
+    wire y;
+    
+    and_gate_b DUT(
+        .a(a),
+        .b(b),
+        .y(y)
+    );
+    
+    initial begin
+        a = 0; b = 0;
+        #50;
+    end
+    
+    initial begin
+        a = 0; b = 0;
+        #50;
+        
+        a = 0; b = 1;
+        #50;
+        
+        a = 1; b = 0;
+        #50;
+        
+        a = 1; b = 1;
+        #50;
+        
+        $finish;
+    end
+endmodule
