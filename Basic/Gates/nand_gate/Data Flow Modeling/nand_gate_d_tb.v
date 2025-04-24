c4351671e337d015509adc98c403cc384cbcd774
seongyojeong <seongyojeong1229@gmail.com>
2025-04-24 23:02:38 +0900

    Create nand_gate_d_tb.v

---
+++ Basic/Gates/nand_gate/Data Flow Modeling/nand_gate_d_tb.v
@@ -0,0 +1,27 @@
+module nand_gate_d_tb;
+    reg a;
+    reg b;
+    wire y;
+
+    nand_gate_d DUT(
+        .a(a),
+        .b(b),
+        .y(y)
+    );
+
+    initial begin
+        a = 0; b = 0;
+        #50;
+        
+        a = 0; b = 1;
+        #50;
+        
+        a = 1; b = 0;
+        #50;
+        
+        a = 1; b = 1;
+        #50;
+        
+        $finish;
+    end
+endmodule
