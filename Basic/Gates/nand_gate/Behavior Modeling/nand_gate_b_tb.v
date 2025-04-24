47909d1adb93ef3016a80bc586da2bace5a8b8dd
seongyojeong <seongyojeong1229@gmail.com>
2025-04-24 23:06:04 +0900

    Create nand_gate_b_tb.v

---
+++ Basic/Gates/nand_gate/Behavior Modeling/nand_gate_b_tb.v
@@ -0,0 +1,27 @@
+module nand_gate_b_tb;
+    reg a;
+    reg b;
+    wire y;
+
+    nand_gate_b DUT(
+        .a(a),
+        .b(b),
+        .y(y)
+    );
+
+    initial begin
+        a = 0; b = 0;
+        #50;
+        
+        a = 0; b = 1;
+        #50;
+        
+        a = 1; b = 0;
+        #50;
+        
+        a = 1; b = 1;
+        #50;
+        
+        $finish;
+    end
+endmodule
