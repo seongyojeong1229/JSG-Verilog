c76c10a95a3e0ef4164f08e5382c5aece629a648
seongyojeong <seongyojeong1229@gmail.com>
2025-05-05 14:56:52 +0900

    Update full_subtractor_s.v

--- Basic/Combinational Logic Circuit/full_subtractor/Structural Modeling/full_subtractor_s.v
+++ Basic/Combinational Logic Circuit/full_subtractor/Structural Modeling/full_subtractor_s.v
@@ -9,7 +9,7 @@ module full_subtractor_s(
     wire s1, c1, c2;
     
     xor x1(s1,a,b);
-    and a1(c1,a,~b);
+    and a1(c1,~a,b);
     xor x2(d,s1,borrow_in);
     and a2(c2,~s1,borrow_in);
     or(borrow_out,c1,c2);
