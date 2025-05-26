e6784f8490aa12d5dc19e01fa3a8779b0c67c34d
seongyojeong <seongyojeong1229@gmail.com>
2025-05-26 13:21:11 +0900

    Update ripple_carry_add_and_sub_4bit_m1.v

--- Basic/Combinational Logic Circuit/ripple_carry_add_and_sub/ripple_carry_add_and_sub_4bit_m1.v
+++ Basic/Combinational Logic Circuit/ripple_carry_add_and_sub/ripple_carry_add_and_sub_4bit_m1.v
@@ -16,9 +16,9 @@ module ripple_carry_add_and_sub_4bit_m1(
     xor x4(bb3,b[3],M);
  
     full_adder_m  fa0(.a(a[0]), .b(bb0), .cin(c_in) , .sum(sum[0]), .cout(c1));
-    full_adder_m  fa1(.a(a[1]), .b(bb1), .cin(c1), .sum(sum[1]), .cout(c2));
-    full_adder_m  fa2(.a(a[2]), .b(bb2), .cin(c2), .sum(sum[2]), .cout(c3));
-    full_adder_m  fa3(.a(a[3]), .b(bb3), .cin(c3), .sum(sum[3]), .cout(c_out));
+    full_adder_m  fa1(.a(a[1]), .b(bb1), .cin(c1),    .sum(sum[1]), .cout(c2));
+    full_adder_m  fa2(.a(a[2]), .b(bb2), .cin(c2),    .sum(sum[2]), .cout(c3));
+    full_adder_m  fa3(.a(a[3]), .b(bb3), .cin(c3),    .sum(sum[3]), .cout(c_out));
  
 endmodule
  
