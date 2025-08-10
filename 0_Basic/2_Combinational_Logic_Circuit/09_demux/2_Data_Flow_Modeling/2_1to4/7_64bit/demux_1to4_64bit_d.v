8588be6d3a2e024cc01077c8e251ab9b11aaa34e
seongyojeong <seongyojeong1229@gmail.com>
2025-08-10 16:26:41 +0900

    modify

--- 0_Basic/2_Combinational_Logic_Circuit/09_demux/2_Data_Flow_Modeling/2_1to4/7_64bit/demux_1to4_64bit_d.v
+++ 0_Basic/2_Combinational_Logic_Circuit/09_demux/2_Data_Flow_Modeling/2_1to4/7_64bit/demux_1to4_64bit_d.v
@@ -7,9 +7,9 @@ module demux_1to4_d #(parameter width = 64)(
     output [width-1:0] o3
 );
 
-    assign o0 = (sel == 2'b00) ? i : 1'b0;
-    assign o1 = (sel == 2'b01) ? i : 1'b0;
-    assign o2 = (sel == 2'b10) ? i : 1'b0;
-    assign o3 = (sel == 2'b11) ? i : 1'b0;
+    assign o0 = (sel == 2'b00) ? i : {width{1'b0}};
+    assign o1 = (sel == 2'b01) ? i : {width{1'b0}};
+    assign o2 = (sel == 2'b10) ? i : {width{1'b0}};
+    assign o3 = (sel == 2'b11) ? i : {width{1'b0}};
 
 endmodule
