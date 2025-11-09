bdbae080cc2e2c625a0a109a1ff2f8a2f5936086
seongyojeong <seongyojeong1229@gmail.com>
2025-11-09 21:52:18 +0900

    rename

--- 0_Basic/2_Combinational_Logic_Circuit/03_Code_Converters/03_priority_encoder/work/vivado_proj/run_synth_and_open.tcl
+++
@@ -1,7 +0,0 @@
-# 1. 프로젝트 열기
-open_project vivado_proj.xpr
-# 2. 'SYNTHESIS -> Run Synthesis' 실행
-launch_runs synth_1 -jobs 7
-wait_on_run synth_1
-# 3. 'SYNTHESIS -> Open Synthesized Design' 실행
-open_run synth_1
